a464abac9c9008ecebfd9491070c869a50bdff87
Kevin Lee <44526583+kevindclee@users.noreply.github.com>
2020-02-09 16:05:47 -0500

    set up http server

--- index.js
+++ index.js
@@ -3,18 +3,9 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    /*
-    if(req.url === '/'){
-        fs.readFile(path.join(__dirname, 'public', 'web.html'), (err, content) => {
-            if(err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
-        })
-    }*/
     // Build fie path
     let filePath = path.join(__dirname, 'public', req.url === '/' ? 'web.html' : req.url);
     console.log(filePath);
-    res.end();
 
     // Extension of file
     let extname = path.extname(filePath);
@@ -41,16 +32,15 @@ const server = http.createServer((req, res) => {
 
     // read file
     fs.readFile(filePath, (err, content) => {
-        if(err){
-            if(err.code == 'ENOENT'){
-                //Page not found
-                
-            }
+        if(err){throw err}
+        else{
+            res.writeHead(200, {'Content-Type': conetentType});
+            res.end(content, 'utf8');
         }
     });
 
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log('Server running on port ${PORT}'));
