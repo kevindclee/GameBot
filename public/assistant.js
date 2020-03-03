11991b9be9efef25ea0ca2e0655d7bf43154bd81
Kevin Lee <44526583+kevindclee@users.noreply.github.com>
2020-03-03 16:08:42 -0500

    added addGame method

--- public/assistant.js
+++ public/assistant.js
@@ -105,6 +105,52 @@ async function addGreeting(){
     }
 }
 
+var platformId = '[48,49,6,130]'
+var genresId = '(11,12,13,14,15,31)'
+
+async function addGames(){
+    try{
+        var request = require('request');
+        //need check respose from assistant to determine which platform
+    
+        var options = {
+          'method': 'POST',
+          'url': 'https://api-v3.igdb.com/games',
+          'headers': {
+            'user-key': '90afd0d7f337b9deafb7e15d5dfa3f62',
+            'Content-Type': 'text/plain'
+          },
+          body: "fields name,popularity,category,platforms,genres;\nwhere category = 0 & platforms = "+platformId+"& genres = "+genresId+" ;sort popularity desc; limit 6;\n"
+    
+        };
+        request(options, function (error, response) {
+          if (error) throw new Error(error);
+          //console.log(response.body);
+          var data = JSON.parse(response.body);
+          console.log(data)
+          console.log(data.length);
+          console.log(data[0].name)
+    
+          //remove before picture
+          var element = document.getElementById("recommendList");
+          element.innerHTML = '';
+          //add game list
+          var i = 0;
+          for (i=0;i<data.length;i++){
+            var node = document.createElement("LI");
+            var textnode = document.createTextNode(data[i].name);
+            node.appendChild(textnode);
+          }
+        });
+
+    }
+    catch (e) {
+        console.log('error', e);
+        return null;
+    }
+
+}
+
 async function addChat(){
     try{
         var client_div = document.createElement('div');
@@ -168,7 +214,9 @@ async function addChat(){
         current[0].appendChild(assistant_div); 
 
         var objDiv = document.getElementById("autoScroll");
-        objDiv.scrollTop = objDiv.scrollHeight;
+        objDiv.scrollTop = objDiv.scrollHeight;    
+    
+        addGames();
     }
     catch (e) {
         console.log('error', e);
